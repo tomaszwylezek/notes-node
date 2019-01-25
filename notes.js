316b9a092bf7e4e25725b19399e55054caf5355a
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-25 10:56:37 +0100

    Allow to add notes

--- notes.js
+++ notes.js
@@ -1,7 +1,28 @@
 console.log("Starting notes.js");
 
+const fs = require("fs");
+
 const addNote = (title, body) => {
   console.log("Adding note", title, body);
+
+  let notes = [];
+  const note = {
+    title,
+    body
+  };
+
+  try {
+    const notesString = fs.readFileSync("notes-data.json");
+    notes = JSON.parse(notesString.toString());
+  } catch (e) {}
+
+  let duplicateNotes = notes.some(note => note.title === title);
+
+  if (!duplicateNotes) {
+    notes.push(note);
+
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+  }
 };
 const getAll = () => {
   console.log("Getting all notes");
