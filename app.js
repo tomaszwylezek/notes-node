13483b725c3e7be50676d23faa288e5f16246311
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-25 11:03:44 +0100

    Refactor code

--- app.js
+++ app.js
@@ -12,14 +12,25 @@ const command = process.argv[2];
 console.log("Command:", command);
 console.log("argv:", argv);
 
-if (command === "add") {
-  notes.addNote(argv.title, argv.body);
-} else if (command === "list") {
-  notes.getAll();
-} else if (command === "read") {
-  notes.getNote(argv.title);
-} else if (command === "remove") {
-  notes.removeNote(argv.title);
-} else {
-  console.log("Command not recognized");
+switch (command) {
+  case "add": {
+    const note = notes.addNote(argv.title, argv.body);
+
+    if (note) {
+      console.log(`${note.title} -- Saved!`);
+    } else {
+      console.log("Nothing saved -- error");
+    }
+    break;
+  }
+  case "read": {
+    notes.getNote(argv.title);
+    break;
+  }
+  case "remove": {
+    notes.removeNote(argv.title);
+    break;
+  }
+  default:
+    console.log("Command not recognized");
 }
--- notes.js
+++ notes.js
@@ -2,26 +2,34 @@ console.log("Starting notes.js");
 
 const fs = require("fs");
 
+const fetchNotes = () => {
+  try {
+    const notesString = fs.readFileSync("notes-data.json");
+    return JSON.parse(notesString.toString());
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveNotes = notes => {
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+};
+
 const addNote = (title, body) => {
   console.log("Adding note", title, body);
 
-  let notes = [];
+  let notes = fetchNotes();
   const note = {
     title,
     body
   };
 
-  try {
-    const notesString = fs.readFileSync("notes-data.json");
-    notes = JSON.parse(notesString.toString());
-  } catch (e) {}
-
   let duplicateNotes = notes.some(note => note.title === title);
 
   if (!duplicateNotes) {
     notes.push(note);
-
-    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+    saveNotes(notes);
+    return note;
   }
 };
 const getAll = () => {
